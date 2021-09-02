9823f0d8c542d1b4c32ef8f0ca039b780513c709
YBYuan007 <85644458+YBYuan007@users.noreply.github.com>
2021-09-02 11:21:35 -0400

    Delete lighthouse-js-fundamentals directory

--- lighthouse-js-fundamentals/conditions.js
+++
@@ -1,8 +0,0 @@
-const raining = true; 
-const cold = false ; 
-if (raining) {console.log("Dont forget your umbrella!");}
-if (cold) {console.log("make sure you pick out a scarf"); }
-
-console.log("now you are ready to go outside")
-
-if (!raining) { console.log("leave your umbrella at home!")}
